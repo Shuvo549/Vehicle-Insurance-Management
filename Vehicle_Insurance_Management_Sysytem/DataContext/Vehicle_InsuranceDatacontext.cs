3f2818388a10a9d659d533f0fe97afe2b01fdb75
Shuvo <sshuvo9718@gmail.com>
2021-10-16 23:53:48 +0600

    Added Project Vehicle Insurance Management

---
+++ Vehicle_Insurance_Management_Sysytem/DataContext/Vehicle_InsuranceDatacontext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vehicle_Insurance_Management_Sysytem.DataContext
+{
+    public class Vehicle_InsuranceDatacontext
+    {
+    }
+}
---
+++ Vehicle_Insurance_Management_Sysytem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vehicle_Insurance_Management_Sysytem.MvcApplication" Language="C#" %>
